541eb3a2c76a4e1c58c4ed47317b6fdafecda794
Liliana <liliana.ubisoft@gmail.com>
2024-10-15 21:15:03 +0100

    ok

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,13 +2,15 @@ import styles from './Contact.module.css';
 import { IoPersonSharp } from "react-icons/io5";
 import { FaPhone } from "react-icons/fa6";
 import { deleteContact } from '../../redux/contactsSlice';
+import { useDispatch } from 'react-redux';
 
 export default function Contact({ contact }) {
+    const dispatch = useDispatch();
     return (
         <li className={styles.item}>
             <div> <IoPersonSharp />  {contact.name}</div>
            <div> <FaPhone /> {contact.number} </div>
-            <button className={styles.button} onClick={() => deleteContact(contact.id)}>Delete</button>
+            <button className={styles.button} onClick={() => dispatch (deleteContact(contact.id))}>Delete</button>
         </li>
     )
 }
